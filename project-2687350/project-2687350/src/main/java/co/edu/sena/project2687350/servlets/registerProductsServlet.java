554b9b4f13083dc339ef3cbe652483af26acad32
VictorNv11 <125565884+VictorNv11@users.noreply.github.com>
2023-05-30 17:43:24 -0500

    Add files via upload

---
+++ project-2687350/project-2687350/src/main/java/co/edu/sena/project2687350/servlets/registerProductsServlet.java
@@ -0,0 +1,8 @@
+package co.edu.sena.project2687350.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet ("")
+public class registerProductsServlet extends HttpServlet {
+}
